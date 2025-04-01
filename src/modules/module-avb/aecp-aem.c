95bef0f1895ab45a4006779d66e889c48fcc67c2
Simon Gapp <simongapp@gmx.de>
2025-04-02 00:28:22 +0200

    #1: Fixes broken implementation due to merge rebase and what not conflicts

--- src/modules/module-avb/aecp-aem.c
+++ src/modules/module-avb/aecp-aem.c
@@ -106,6 +106,54 @@ static int handle_lock_entity(struct aecp *aecp, const void *m, int len)
 		return reply_not_implemented(aecp, m, len);
 
 	return reply_success(aecp, m, len);
+	#else // USE_MILAN
+	struct server *server = aecp->server;
+	const struct avb_packet_aecp_aem *p = m;
+	const struct avb_packet_aecp_aem_acquire *ae;
+	const struct descriptor *desc;
+	uint16_t desc_type, desc_id;
+	uint32_t flags;
+	uint64_t controller_id;
+	// Store the currently locked controller ID
+	static uint64_t locked_id = 0;
+
+    const struct avb_packet_aecp_aem_lock *le = (const struct avb_packet_aecp_aem_lock*)p->payload;
+	controller_id = be64toh(le->locked_guid);
+	flags = ntohl(le->flags);
+
+	desc_type = ntohs(le->descriptor_type);
+	desc_id = ntohs(le->descriptor_id);
+
+	desc = server_find_descriptor(server, desc_type, desc_id);
+	if (desc == NULL)
+		return reply_status(aecp, AVB_AECP_AEM_STATUS_NO_SUCH_DESCRIPTOR, p, len);
+
+	// Milan v1.2: Sec. 5.4.2.2 LOCK_ENTITY
+	if (desc_type != AVB_AEM_DESC_ENTITY || desc_id != 0)
+		return reply_not_supported(aecp, m, len);	
+
+	// If no one as locked it, assign the current controller
+	if (locked_id == 0)
+	{
+		locked_id = controller_id;
+		// The locked_id field is set to zero (0) for a command, 
+		// and is set to the Entity ID of the AVDECC Controller 
+		// that is holding the lock in a response.
+		return reply_lock_entity(aecp, m, len, locked_id);
+	}
+	// Entity locked but locking controller wants to UNLOCK
+	else if (locked_id == controller_id) && (flags == 1) 
+	{
+		locked_id = 0;
+		return reply_lock_entity(aecp, m, len, locked_id);
+	}
+	
+	// The locked_id field is set to zero (0) for a command, and is set to the Entity ID
+	// of the AVDECC Controller that is holding the lock in a response.
+	// Controller locked
+	return reply_lock_entity(aecp, m, len, locked_id);
+
+	#endif //USE_MILAN
 }
 
 /* ENTITY AVAILABLE according to the locking state */
