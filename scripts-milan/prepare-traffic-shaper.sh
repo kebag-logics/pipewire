f9cddd10ff38e9230b475ac43121e4c75b7db0e7
hackerman-kl <hakerman-kl@kebag-logic.com>
2025-05-06 06:40:52 +0200

    #68 talker: preparation scripts

---
+++ scripts-milan/prepare-traffic-shaper.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Copyright © 2022 Kebag-Logic */
+# SPDX-FileCopyrightText: Copyright © 2025 Alexandre Malki <alexandre.malki@kebag-logic.com>
+# SPDX-License-Identifier: MIT
+
+# Some of hte code is taken from https://tsn.readthedocs.io/
+NIC=${1}
+
+# Clean Everything first
+sudo tc qdisc del dev ${NIC} parent root handle 666 mqprio \
+	num_tc 3 \
+	map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
+	queues 1@0 1@1 2@2 \
+	hw 0 > /dev/null  2>&1
+
+# Tune up the system
+sysctl -w net.core.default_qdisc=pfifo_fast
+sudo sysctl -w net.core.wmem_max=90299200
+sudo sysctl -w net.core.wmem_default=90299200
+
+# Big Big assumption is that the system is running a i210/226
+sudo modprobe -r igb
+sudo modprobe igb
+
+# Increase the number of descriptor to be used
+ethtool -G ${NIC} rx 4096
+ethtool -G ${NIC} tx 4096
+
+# Create the MQPrio mapping to Traffic class to Queue
+sudo tc qdisc add dev ${NIC} parent root handle 666 mqprio \
+	num_tc 3 \
+	map 2 2 1 0 2 2 2 2 2 2 2 2 2 2 2 2 \
+	queues 1@0 1@1 2@2 \
+	hw 0
+
+# Setup the QDisc for the traffic shapper
+# The qdisc value here is set to transmit ONLY 1 Stream
+# Calculation are done accordingly to https://tsn.readthedocs.io/qdiscs.html#configuring-cbs-qdisc
+sudo tc qdisc replace dev ${NIC} parent 666:1 cbs \
+	idleslope 98688 sendslope -901312 hicredit 153 locredit -1389 \
+	offload 1
+
+# Set up the ETF for a 125us tx time
+sudo tc qdisc add dev ${NIC} parent 666:1 etf \
+	clockid CLOCK_TAI \
+	delta 500000 \
+	offload
+
+tc qdisc show dev ${NIC}
---
+++ scripts-milan/ptp-start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Copyright © 2022 Kebag-Logic */
+# SPDX-FileCopyrightText: Copyright © 2025 Alexandre Malki <alexandre.malki@kebag-logic.com>
+# SPDX-License-Identifier: MIT
+
+# part of the code taken from https://tsn.readthedocs.io/
+function kill_all()
+{
+	sudo pkill ptp4l
+	sudo pkill phc2sys
+	sudo timedatectl set-ntp true
+}
+
+trap 'kill_all' SIGINT
+
+# fopr
+sudo timedatectl set-ntp false
+sudo ptp4l -i enp8s0 -f /home/alex/prjs/linuxptp/configs/gPTP.cfg --step_threshold=1 &
+PTP_PID=$$
+
+sudo phc2sys -s enp8s0 -c CLOCK_REALTIME --step_threshold=1 --transportSpecific=1 -w -m &
+PHC2SYS_PID=$$
+
+sudo pmc -u -b 0 -t 1 "SET GRANDMASTER_SETTINGS_NP clockClass 248 \
+	clockAccuracy 0xfe offsetScaledLogVariance 0xffff \
+	currentUtcOffset 37 leap61 0 leap59 0 currentUtcOffsetValid 1 \
+	ptpTimescale 1 timeTraceable 1 frequencyTraceable 0 \
+	timeSource 0xa0"
+
+waitpid $PTP_PID $PHC2SYS_PID
+
+kill_all
