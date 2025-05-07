2f304ef4fa1502f4b486819c85e76a244dc632c9
Simon Gapp <simongapp@gmx.de>
2025-05-07 22:52:03 +0000

    #72: fixes unseen merge conflicts

--- scripts-milan/ptp-start.sh
+++ scripts-milan/ptp-start.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright © 2022 Kebag-Logic */
+# SPDX-FileCopyrightText: Copyright © 2022 Kebag-Logic
 # SPDX-FileCopyrightText: Copyright © 2025 Alexandre Malki <alexandre.malki@kebag-logic.com>
 # SPDX-FileCopyrightText: Copyright © 2025 Simon Gapp <simon.gapp@kebag-logic.com>
 # SPDX-License-Identifier: MIT
 
-# part of the code taken from https://tsn.readthedocs.io/
+# Part of the code taken from https://tsn.readthedocs.io/
 
-<<<<<<< Updated upstream
-NIC=${1}
-=======
 if [ $# -lt 1 ]; then
     echo "Usage: $0 <network-interface>"
     echo "Selected interface: (none)"
@@ -20,45 +17,34 @@ fi
 IFACE="$1"
 echo "Selected interface: $IFACE"
 
->>>>>>> Stashed changes
-function kill_all()
-{
+function kill_all() {
+    echo "Stopping PTP services..."
     sudo pkill ptp4l
     sudo pkill phc2sys
     sudo timedatectl set-ntp true
 }
 
-trap 'kill_all' SIGINT
+trap kill_all SIGINT EXIT
 
 sudo timedatectl set-ntp false
-<<<<<<< Updated upstream
-sudo ptp4l -i ${NIC} -f /home/alex/prjs/linuxptp/configs/gPTP.cfg --step_threshold=1 &
-PTP_PID=$$
-sudo chrt -f -p 53 $PTP_PID
-=======
+
+# Start ptp4l
 sudo ptp4l -i "$IFACE" -f ~/linuxptp/configs/gPTP.cfg --step_threshold=1 &
 PTP_PID=$!
+sudo chrt -f -p 53 "$PTP_PID"
 
+# Start phc2sys
 sudo phc2sys -s "$IFACE" -c CLOCK_REALTIME --step_threshold=1 --transportSpecific=1 -w -m &
 PHC2SYS_PID=$!
->>>>>>> Stashed changes
+sudo chrt -f -p 52 "$PHC2SYS_PID"
 
+# Set grandmaster settings
 sudo pmc -u -b 0 -t 1 "SET GRANDMASTER_SETTINGS_NP clockClass 248 \
     clockAccuracy 0xfe offsetScaledLogVariance 0xffff \
     currentUtcOffset 37 leap61 0 leap59 0 currentUtcOffsetValid 1 \
     ptpTimescale 1 timeTraceable 1 frequencyTraceable 0 \
     timeSource 0xa0"
 
-<<<<<<< Updated upstream
-sudo chrt -f -p 52 $PTP_PID
-sudo phc2sys -s ${NIC} -c CLOCK_REALTIME --step_threshold=1 --transportSpecific=1 -w -m &
-PHC2SYS_PID=$$
-
-
-waitpid $PTP_PID $PHC2SYS_PID
-=======
-wait $PTP_PID
-wait $PHC2SYS_PID
->>>>>>> Stashed changes
-
-kill_all
+# Wait for background processes
+wait "$PTP_PID"
+wait "$PHC2SYS_PID"
